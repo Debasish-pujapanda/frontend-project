2524a14624917a2527658086ed38b54bb0f8da36
Debasish-pujapanda <81896070+Debasish-pujapanda@users.noreply.github.com>
2021-09-03 12:55:06 +0530

    Add files via upload

---
+++ java script project/questions.js
@@ -0,0 +1,143 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+
+
+  {
+    numb: 6,
+    question: "What does CPU stand for?",
+    answer: "central processing unit",
+    options: [
+      "control processing unit",
+      "coordinate price unit",
+      "central processing unit",
+      "collision process unit"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "What does WWW stand for?",
+    answer: "World wide web",
+    options: [
+      "wood wide web",
+      "World wide web",
+      "wools willis web",
+      "wild wide web"
+    ]
+  },
+
+
+  
+  {
+    numb: 8,
+    question: "What does API stand for?",
+    answer: "Application programming interface",
+    options: [
+      "Audio programming interface",
+      "Alt programming interface",
+      "App program interface",
+      "Application programming interface"
+    ]
+  },
+
+  
+  {
+    numb: 9,
+    question: "What does ROM stand for?",
+    answer: "Read only memory",
+    options: [
+      "Read only memory",
+      "Real only memory",
+      "Reel only memory",
+      "Random only memory"
+    ]
+  },
+
+
+
+  
+  {
+    numb: 10,
+    question: "What does RAM stand for?",
+    answer: "Random access memory",
+    options: [
+      "Read access memory",
+      "Real axis memory",
+      "Random access memory",
+      "Random accumulate memory"
+    ]
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
